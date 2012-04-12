946e0ca97c20786a3f32f3ea9494822c2b2f65c1
Ranjit Vadakkan <ranjit.vadakkan@gmail.com>
2012-04-12 14:34:13 -0400

    simple parent project done

---
+++ simple-parent/simple-webapp/src/main/java/org/sonatype/mavenbook/web/WeatherServlet.java
@@ -0,0 +1,24 @@
+package org.sonatype.mavenbook.web;
+
+import java.io.*;
+import javax.servlet.*;                                                         
+import javax.servlet.http.*;
+import org.sonatype.mavenbook.weather.WeatherService;
+
+public class WeatherServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws ServletException, IOException {
+   
+        String zip = request.getParameter("zip" );
+        WeatherService weatherService = new WeatherService(); 
+        PrintWriter out = response.getWriter();
+	try {
+            out.println( weatherService.getForecast( zip ) );
+        } catch( Exception e ) {
+            out.println( "Error Retrieving Forecast: " + e.getMessage() );
+        }
+        out.flush();
+        out.close();
+    }
+}
